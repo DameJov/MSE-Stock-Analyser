db1d899519aeb6b2f133f4f567307ed1568b453a
DameJov <jov.damjan@gmail.com>
2025-01-19 20:28:31 +0100

    Commit

--- src/main/java/mk/finki/ukim/mk/mseanalyiser/MseAnalyiserApplication.java
+++ src/main/java/mk/finki/ukim/mk/mseanalyiser/MseAnalyiserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MseAnalyiserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MseAnalyiserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MseAnalyiserApplication.class, args);
+    }
 
 }
--- src/main/java/mk/finki/ukim/mk/mseanalyiser/repository/inmemory/InMemoryCodeRepository.java
+++ src/main/java/mk/finki/ukim/mk/mseanalyiser/repository/inmemory/InMemoryCodeRepository.java
@@ -1,4 +1,13 @@
 package mk.finki.ukim.mk.mseanalyiser.repository.inmemory;
 
+import mk.finki.ukim.mk.mseanalyiser.bootstrap.DataHolder;
+import mk.finki.ukim.mk.mseanalyiser.model.Codes;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class InMemoryCodeRepository {
+    public Codes getCode()
+    {
+        return DataHolder.codes.get(0);
+    }
 }
